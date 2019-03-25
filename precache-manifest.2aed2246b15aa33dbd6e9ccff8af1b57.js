852cc57f83a89d5fe13820459809c8220c4659c3
Nathan Han <nathan0713@gmail.com>
2019-03-25 16:34:58 -0700

    Updates

---
+++ precache-manifest.2aed2246b15aa33dbd6e9ccff8af1b57.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "16cee1c24b28926f0cf105333f5f33e8",
+    "url": "/portfolio/static/media/attFriends.16cee1c2.png"
+  },
+  {
+    "revision": "c70e7105dae10d9bec7b",
+    "url": "/portfolio/static/css/main.31812e84.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "b29b08232232af30465a",
+    "url": "/portfolio/static/js/2.7efcb614.chunk.js"
+  },
+  {
+    "revision": "462bb310e06c67cb911b1afa77d3269e",
+    "url": "/portfolio/static/media/seattle-min.462bb310.png"
+  },
+  {
+    "revision": "f0b496cdc7c5ddd2a904bb3297fc631c",
+    "url": "/portfolio/static/media/textbookFriends-min.f0b496cd.PNG"
+  },
+  {
+    "revision": "85affbe9d2e868bd7d23ac7912edd506",
+    "url": "/portfolio/static/media/grailz.85affbe9.png"
+  },
+  {
+    "revision": "c70e7105dae10d9bec7b",
+    "url": "/portfolio/static/js/main.8852ba4f.chunk.js"
+  },
+  {
+    "revision": "bc1c262ec70e08e53600e429e4883e38",
+    "url": "/portfolio/static/media/shredderz.bc1c262e.png"
+  },
+  {
+    "revision": "20f85afcb7bebc27dd9a1c04bd9cdb2f",
+    "url": "/portfolio/static/media/LaserCut1.20f85afc.jpg"
+  },
+  {
+    "revision": "cf4df5541bb1e94ee94ddeebff323165",
+    "url": "/portfolio/static/media/3DPrint.cf4df554.jpg"
+  },
+  {
+    "revision": "7fcb30ee1b75372a81cfd151fb705d57",
+    "url": "/portfolio/static/media/HomeFantasyFootball.7fcb30ee.PNG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "64f2a5feca6ebb6d204b6a0b713dfb05",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.34f9ab45921f8abec9dc46cdd8e0d500.js"
+  "/portfolio/precache-manifest.2aed2246b15aa33dbd6e9ccff8af1b57.js"
 );
 
 workbox.clientsClaim();
