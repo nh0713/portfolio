de1bd0536f82196e05744b84286c6fa338e4b3ba
Nathan Han <nathan0713@gmail.com>
2019-03-26 15:06:48 -0700

    router

--- src/App.js
+++ src/App.js
@@ -10,18 +10,18 @@ import AboutMe from './components/AboutMe'
 class App extends Component {
   render() {
     return (
-      // <Router>
-      //   <Switch>
-      //     <Route path='/portfolio/' component={HomePage} exact />
-      //     <Route path='/portfolio/AboutMe' component={AboutMe} exact />
-      //   </Switch>
-      // </Router>
       <Router>
-      <Switch>
-        <Route path='/' component={HomePage} exact />
-        <Route path='/AboutMe' component={AboutMe} exact />
-      </Switch>
-    </Router>
+        <Switch>
+          <Route path='/portfolio/' component={HomePage} exact />
+          <Route path='/portfolio/AboutMe' component={AboutMe} exact />
+        </Switch>
+      </Router>
+    //   <Router>
+    //   <Switch>
+    //     <Route path='/' component={HomePage} exact />
+    //     <Route path='/AboutMe' component={AboutMe} exact />
+    //   </Switch>
+    // </Router>
       // <div className="App">
       //   <header className="App-header">
       //     <HomePage />
