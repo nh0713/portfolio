e4a69ccaa8b542100c14a3e86133c16c5baf5eca
Nathan Han <nathan0713@gmail.com>
2019-04-02 15:53:42 -0700

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { HashRouter as Router, Route, Switch} from 'react-router-dom'
 
 import HomePage from './components/HomePage.js'
 import AboutMe from './components/AboutMe'
