6ea3bc639e892929105313abe6e127e050685794
Nathan Han <nathan0713@gmail.com>
2019-04-03 00:19:14 -0700

    Updates

---
+++ precache-manifest.ef7ab63c2c807d4eb0a992559679de66.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc1c262ec70e08e53600e429e4883e38",
+    "url": "/portfolio/static/media/shredderz.bc1c262e.png"
+  },
+  {
+    "revision": "c4956757b6d497fccf70",
+    "url": "/portfolio/static/css/main.8b3b8aa3.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "4b2e70349ed829c3bc36",
+    "url": "/portfolio/static/js/2.87782492.chunk.js"
+  },
+  {
+    "revision": "be8e489ef8b3933085afa95b53bd0508",
+    "url": "/portfolio/static/media/seattle-min-resized.be8e489e.png"
+  },
+  {
+    "revision": "483500e187e3d6aca19178fc332d001b",
+    "url": "/portfolio/static/media/textbookFriends.483500e1.PNG"
+  },
+  {
+    "revision": "85affbe9d2e868bd7d23ac7912edd506",
+    "url": "/portfolio/static/media/grailz.85affbe9.png"
+  },
+  {
+    "revision": "16cee1c24b28926f0cf105333f5f33e8",
+    "url": "/portfolio/static/media/attFriends.16cee1c2.png"
+  },
+  {
+    "revision": "c4956757b6d497fccf70",
+    "url": "/portfolio/static/js/main.2e38ca39.chunk.js"
+  },
+  {
+    "revision": "7fcb30ee1b75372a81cfd151fb705d57",
+    "url": "/portfolio/static/media/HomeFantasyFootball.7fcb30ee.PNG"
+  },
+  {
+    "revision": "374d0d3ef213b086097bddc9822a7f64",
+    "url": "/portfolio/static/media/att.374d0d3e.png"
+  },
+  {
+    "revision": "ed2a4fc19d5bb5504a8712d5d10b9995",
+    "url": "/portfolio/static/media/cwk.ed2a4fc1.png"
+  },
+  {
+    "revision": "f58dda45e1fd910491b884c5d579513c",
+    "url": "/portfolio/static/media/ftl.f58dda45.jpg"
+  },
+  {
+    "revision": "a4ca1dfa2a562fabb4fb3cf943ffb213",
+    "url": "/portfolio/static/media/wireframes.a4ca1dfa.png"
+  },
+  {
+    "revision": "874c29333798f1c61f4f63c033269b97",
+    "url": "/portfolio/static/media/affinity.874c2933.jpg"
+  },
+  {
+    "revision": "1eafac5ad02eca0f7a937007c5c0b324",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e3f73430208f67783e47c2bf029adb37.js"
+  "/portfolio/precache-manifest.ef7ab63c2c807d4eb0a992559679de66.js"
 );
 
 workbox.clientsClaim();
