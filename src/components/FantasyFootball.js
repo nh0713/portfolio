7e5f2d4b15ccc2b0570c1128d4be904600aca078
Nathan Han <nathan0713@gmail.com>
2019-04-04 21:26:16 -0700

    typo

--- src/components/FantasyFootball.js
+++ src/components/FantasyFootball.js
@@ -33,9 +33,9 @@ class FantasyFootball extends Component {
                 <Container fluid className="outerContainer">
                     <Container className='projectDetails'>
                         <h2>Purpose</h2>
-                        <p>I created this web application as a final project for a class. The purpose of this web application was to allow people to compare stats of NFL players for Fantasy Football. Users can compare two different players and can filter both on a specific week or stat. It pulled JSON data from an API, however, the API is no longer free. As a result, the comparison functino no longer works.</p>
-                        <h2>What I learned</h2>
-                        <p></p>
+                        <p>I created this web application as a final project for a class. The purpose of this web application was to allow people to compare stats of NFL players for Fantasy Football. Users can compare two different players and can filter both on a specific week or stat. It pulled JSON data from an API, however, unfortunately the API is no longer free. As a result, the comparison function no longer works.</p>
+                        {/* <h2>What I learned</h2>
+                        <p></p> */}
                     </Container>
                     <ImgCarousel />
                 </Container>
