fb59791293162c1764e15b4d7ea8cfe976b0ff4b
Nathan Han <nathan0713@gmail.com>
2019-11-19 14:01:16 -0800

    Updates

---
+++ precache-manifest.fd607e0e1ef2760879073d6520d19795.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662cb95247740951c25a2abb6b1404b2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3ab79a305daf3b550d13",
+    "url": "/portfolio/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "1bc23b8ab11956c103c5",
+    "url": "/portfolio/static/css/main.2c73f8dd.chunk.css"
+  },
+  {
+    "revision": "3ab79a305daf3b550d13",
+    "url": "/portfolio/static/js/2.b64c96d6.chunk.js"
+  },
+  {
+    "revision": "1bc23b8ab11956c103c5",
+    "url": "/portfolio/static/js/main.82592832.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  },
+  {
+    "revision": "2244012dea36222150e17a1448bb96e6",
+    "url": "/portfolio/static/media/FantasyFootballFilledOut.2244012d.PNG"
+  },
+  {
+    "revision": "ac8b73c7e314d0f18b7447f4f8f23ad9",
+    "url": "/portfolio/static/media/FantasyFootballResults2.ac8b73c7.png"
+  },
+  {
+    "revision": "7fcb30ee1b75372a81cfd151fb705d57",
+    "url": "/portfolio/static/media/HomeFantasyFootball.7fcb30ee.PNG"
+  },
+  {
+    "revision": "b661eeb55e7bdd8b1fe69a27a59d7f7c",
+    "url": "/portfolio/static/media/ProcessBook.b661eeb5.pdf"
+  },
+  {
+    "revision": "874c29333798f1c61f4f63c033269b97",
+    "url": "/portfolio/static/media/affinity.874c2933.jpg"
+  },
+  {
+    "revision": "16cee1c24b28926f0cf105333f5f33e8",
+    "url": "/portfolio/static/media/attFriends.16cee1c2.png"
+  },
+  {
+    "revision": "5140ca0ba4a8f9917e9dcc1cfefd5ea8",
+    "url": "/portfolio/static/media/basketball.5140ca0b.jpg"
+  },
+  {
+    "revision": "ab1bf6e37a471d77b6e5c008eed7932c",
+    "url": "/portfolio/static/media/catScreenshot.ab1bf6e3.PNG"
+  },
+  {
+    "revision": "58881174f379868d5edd2772ad000265",
+    "url": "/portfolio/static/media/code.58881174.jpg"
+  },
+  {
+    "revision": "85affbe9d2e868bd7d23ac7912edd506",
+    "url": "/portfolio/static/media/grailz.85affbe9.png"
+  },
+  {
+    "revision": "0bf383849a9a4cd44e657651dc82ba55",
+    "url": "/portfolio/static/media/r8Designmin.0bf38384.png"
+  },
+  {
+    "revision": "bb019103a50c05c64b11554f8c35268d",
+    "url": "/portfolio/static/media/results1.bb019103.png"
+  },
+  {
+    "revision": "5fb120b4e4eff0abc2bb0aa58949290b",
+    "url": "/portfolio/static/media/resume.5fb120b4.png"
+  },
+  {
+    "revision": "be8e489ef8b3933085afa95b53bd0508",
+    "url": "/portfolio/static/media/seattle-min-resized.be8e489e.png"
+  },
+  {
+    "revision": "bc1c262ec70e08e53600e429e4883e38",
+    "url": "/portfolio/static/media/shredderz.bc1c262e.png"
+  },
+  {
+    "revision": "478b2da5ac73a24c3e820b8c3eaf1728",
+    "url": "/portfolio/static/media/stickShift.478b2da5.jpg"
+  },
+  {
+    "revision": "483500e187e3d6aca19178fc332d001b",
+    "url": "/portfolio/static/media/textbookFriends.483500e1.PNG"
+  },
+  {
+    "revision": "05d1b8a96537edc4fab54f8bcf4a1a5d",
+    "url": "/portfolio/static/media/videoGames.05d1b8a9.jpg"
+  },
+  {
+    "revision": "a4ca1dfa2a562fabb4fb3cf943ffb213",
+    "url": "/portfolio/static/media/wireframes.a4ca1dfa.png"
+  }
+]);
